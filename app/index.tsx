762875ec70a28b6ca3fa25fb9c6836f536d43cd0
Shahid-projects <shahidfakir48@gmail.com>
2025-06-30 17:10:07 +0530

    first Task

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+
+const Index = () => {
+  return <View
+  style={{
+      backgroundColor: "black",
+      flex:1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+    <Text style={{
+      color:"white",
+      fontSize: 50,
+
+    }}>
+      shahid Fakir
+    </Text>
+  </View>
+}
+
+export default Index;
